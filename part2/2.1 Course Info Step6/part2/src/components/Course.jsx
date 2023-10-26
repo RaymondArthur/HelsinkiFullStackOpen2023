70b2b097102b34ed68370b2e1119bf271827dc0f
RaymondArthur <raymond.g.assaad@gmail.com>
2023-10-26 12:51:19 +1100

    commit for exercise 2.1

---
+++ part2/2.1 Course Info Step6/part2/src/components/Course.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map((p)=><Part key={p.id} part={p}/>)}    
+  </>
+
+const Course = (props) =>{
+    console.log(props)
+
+    return (
+        <>
+            <Header course={props.course.name} />
+            <Content parts={props.course.parts} />
+        </>
+    )
+}
+
+export default Course
