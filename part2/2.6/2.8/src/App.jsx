8b3f0e3c877ef8dba7d5a2fba20df091ef80030e
RaymondArthur <raymond.g.assaad@gmail.com>
2023-11-02 17:36:30 +1100

    implemented 2.8, added a phone input field. Also, started refactoring components.

--- part2/2.6/2.8/src/App.jsx
+++ part2/2.6/2.8/src/App.jsx
@@ -1,19 +1,47 @@
 import { useState } from 'react'
 
-const InputField = (props) => {
+const SubHeading = ({text}) => {
+  return (
+    <div>
+      <h2>{text}</h2>
+    </div>
+  )
+}
+
+const InputGroup = (props) => {
 
   return (
     <div>
-      
+        {props.formLabel}: <input defaultValue={props.defaultValue} onChange={props.handleChange} />
+    </div>
+  )
+}
+
+const Person = ({name,number}) => {
+  return (
+    <div>
+      <li key={name}>{name} {number}</li>
+    </div>
+  )
+}
+
+const PhoneBookList = ({persons}) => {
+
+  return (
+    <div>
+      <ul>
+        {persons.map(p=><Person key={p.name} name={p.name} number={p.number} />)}
+      </ul>
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number:'040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhonenumber, setNewPhonenumber] = useState('')
 
   //objectsAreEqual() is used to check if two objects are 'equal'
   const objectsAreEqual = (obj1,obj2) => {
@@ -52,10 +80,10 @@ const App = () => {
     return true
   }
 
-  const objInArray = (arr,obj) => {
+  const personInArray = (arr,obj) => {
     
     for(let i = 0; i < arr.length; i++){
-      if(objectsAreEqual(arr[i],obj)){
+      if(objectsAreEqual(arr[i].name,obj.name)){
         return true //obj is already in array, hence return true
       }
     }
@@ -68,23 +96,29 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  //adding the newName dtate to the persons state 
+  //saving the new phone numbe from the form into the newPhonenumber state
+  const handleNumberChange = (event) => {
+    setNewPhonenumber(event.target.value)
+  }
+
+  //adding the newName state to the persons state 
   const addNewPerson = (event) => {
 
     event.preventDefault() //stops the form submit from reloading the page
     const newNameObject = {
-      name: newName
+      name: newName,
+      number: newPhonenumber
     }
     
     //we don't allow blank strings
     if(newNameObject.name !== ""){
       //we don't allow duplicate names to be added (we ARE case sensitive)
-      if(objInArray(persons,newNameObject) === false){
+      if(personInArray(persons,newNameObject) === false){
         //we use Array.concat() to add the newName state to the existing persons array
         //recall that concat() returns a NEW array
         setPersons(persons.concat(newNameObject))
       } else {
-        alert(`${newName}$ is already added to the phonebook`)
+        alert(`${newName} is already added to the phonebook`)
       }
     } else {
       alert("Please enter a non-empty name to add to the dictionary")
@@ -92,26 +126,25 @@ const App = () => {
     
     //resetting the newName state
     setNewName('')
+    setNewPhonenumber('')
   }
 
   return (
     
     <div>
-      <h2>Phonebook</h2>
+      <SubHeading text="Phonebook" />
 
+      
       <form onSubmit={addNewPerson}>
-        <div>
-          name: <input defaultValue={newName} onChange={handleNameChange}/>
-        </div>
+        <InputGroup formLabel="name" value={newName} handleChange={handleNameChange}  />
+        <InputGroup formLabel="number" value={newPhonenumber} handleChange={handleNumberChange}  />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
 
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(p=><li key={p.name}>{p.name}</li>)}
-      </ul>
+      <SubHeading text="Numbers" />
+      <PhoneBookList persons={persons} />
     </div>
   )
 }
